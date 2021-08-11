c7d53d07fa3ab4e978a2565f655ec5f4d57a0d23
rkota <rosairekota@gmail.com>
2021-08-11 12:23:03 +0100

    feat:configure swagger openApi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //configure pipe Validations
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // configure swagger Module
+  const config = new DocumentBuilder()
+    .setTitle('API SANGO NA SMS')
+    .setDescription('Rappel par sms des dates de rendez-vous des vaccinations')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.APP_PORT || 3080);
 }
 bootstrap();

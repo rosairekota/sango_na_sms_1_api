f572ee499f9140bf7acdd67565383280dbc8fd67
rkota <rosairekota@gmail.com>
2021-08-11 12:38:13 +0100

    feat:initial commit

---
+++ dist/app.module.js
@@ -0,0 +1,41 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const typeorm_1 = require("@nestjs/typeorm");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
+const dotenv = require("dotenv");
+dotenv.config();
+let AppModule = class AppModule {
+};
+AppModule = __decorate([
+    common_1.Module({
+        imports: [
+            config_1.ConfigModule.forRoot({
+                isGlobal: true,
+            }),
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'mysql',
+                host: process.env.MYSQL_ADDON_HOST,
+                port: parseInt(process.env.MYSQL_ADDON_PORT),
+                username: process.env.MYSQL_ADDON_USER,
+                password: process.env.MYSQL_ADDON_PASSWORD,
+                database: process.env.MYSQL_ADDON_DB,
+                entities: ['dist/**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }),
+        ],
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
+    })
+], AppModule);
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map

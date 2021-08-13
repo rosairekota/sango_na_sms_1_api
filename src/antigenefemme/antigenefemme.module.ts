225189032f2759a51c141077ae40d2d34695df3c
unknown <FranckTshiz27>
2021-08-13 17:49:33 +0100

    feat : add antigen wife controller

--- src/antigenefemme/antigenefemme.module.ts
+++ src/antigenefemme/antigenefemme.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AntigenefemmeService } from './antigenefemme.service';
 import { AntigenefemmeController } from './antigenefemme.controller';
+import { AntigenefemmeEntity } from './antigenefemme.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AntigenefemmeEntity])],
   providers: [AntigenefemmeService],
-  controllers: [AntigenefemmeController]
+  controllers: [AntigenefemmeController],
 })
 export class AntigenefemmeModule {}
--- src/antigenefemme/antigenefemme.service.ts
+++ src/antigenefemme/antigenefemme.service.ts
@@ -1,4 +1,34 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AntigenefemmeEntity } from './antigenefemme.entity';
+import { AddAntigenWifeDto } from './dto/add_antigenWife.dto';
+import { UpdateAntigenWifeDto } from './dto/update_anitgenWife.dto';
 
 @Injectable()
-export class AntigenefemmeService {}
+export class AntigenefemmeService {
+    constructor(
+        @InjectRepository(AntigenefemmeEntity)
+        private wifeAntigeneRepository : Repository<AntigenefemmeEntity>
+    ){}
+
+    async findAntigenes():Promise<AntigenefemmeEntity[]>{
+        return await this.wifeAntigeneRepository.find();
+    }
+
+    async addAnitgene(antigen:AddAntigenWifeDto):Promise<AntigenefemmeEntity>{
+        return await this.wifeAntigeneRepository.save(antigen);
+    }
+
+    async updateAntigen(idAntigenWife:number,editedAntigen:UpdateAntigenWifeDto): Promise<AntigenefemmeEntity>{
+
+        const antigen = await this.wifeAntigeneRepository.preload({idAntigenWife,...editedAntigen})
+        return await this.wifeAntigeneRepository.save(antigen);
+    }
+
+    async deleteAntigene(idAntigenWife:number):Promise<AntigenefemmeEntity>{
+        const deleteWife = await this.wifeAntigeneRepository.findOne(idAntigenWife);
+        return await this.wifeAntigeneRepository.remove(deleteWife);
+    }
+}

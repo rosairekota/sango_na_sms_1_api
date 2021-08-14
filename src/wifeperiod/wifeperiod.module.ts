aadb02cd2c4d206a1652ba793f924589c25c0ddc
unknown <FranckTshiz27>
2021-08-14 19:28:03 +0100

    fix : module wifeperiod

--- src/wifeperiod/wifeperiod.module.ts
+++ src/wifeperiod/wifeperiod.module.ts
@@ -1,8 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WifeperiodController } from './wifeperiod.controller';
+import { WifeperiodEntity } from './wifeperiod.entity';
 import { WifeperiodService } from './wifeperiod.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([WifeperiodEntity])],
   controllers: [WifeperiodController],
   providers: [WifeperiodService]
 })

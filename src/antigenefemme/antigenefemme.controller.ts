06fa250b84cd8db9e5e2dee1ceabdef573550343
unknown <FranckTshiz27>
2021-08-14 13:45:58 +0100

    fix : remove unesused library

--- src/antigenefemme/antigenefemme.controller.ts
+++ src/antigenefemme/antigenefemme.controller.ts
@@ -4,7 +4,6 @@ import { Repository } from 'typeorm';
 import { AntigenefemmeEntity } from './antigenefemme.entity';
 import { AntigenefemmeService } from './antigenefemme.service';
 import { ApiTags } from '@nestjs/swagger';
-import { IdSelector } from '@reduxjs/toolkit';
 import { FemmeEntity } from 'src/femme/femme.entity';
 import { Get } from '@nestjs/common';
 import { AddAntigenWifeDto } from './dto/add_antigenWife.dto';

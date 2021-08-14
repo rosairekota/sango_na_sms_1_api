5e9169b3ba3d2da3dda6e1c10399512a9bcbe473
unknown <FranckTshiz27>
2021-08-14 12:10:21 +0100

    feat : add Delete and Patch enpoints in centre controller

--- src/centre/centre.controller.ts
+++ src/centre/centre.controller.ts
@@ -1,12 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CentreEntity } from './centre.entity';
 import { CentreService } from './centre.service';
 import { AddCentreDto } from './dto/add-centre.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { UpdateCentreDto } from './dto/update-centre.dto';
 
-@ApiTags('centres:')
-
+@ApiTags('Centres:')
 @Controller('api/centre')
 export class CentreController {
     constructor(
@@ -14,12 +14,22 @@ export class CentreController {
     ){}
 
     @Post()
-  async AddCentre(@Body() centre: AddCentreDto): Promise<CentreEntity> {
-    return await this.centreService.addCentre(centre);
+    async AddCentre(@Body() centre: AddCentreDto): Promise<CentreEntity> {
+      return await this.centreService.addCentre(centre);
+      }
+
+    @Get()
+    async getCentres(): Promise<CentreEntity[]> {
+      return await this.centreService.getAllCenters();
+      }
+
+    @Patch('/:id')
+    async edit(@Param('id',ParseIntPipe) id : number, editedCentre : UpdateCentreDto) : Promise<CentreEntity>{
+      return await this.centreService.update(id,editedCentre);
     }
 
-  @Get('centres')
-  async getCentres(): Promise<CentreEntity[]> {
-    return await this.centreService.getAllCenters();
+    @Delete('/:id')
+    async remove(@Param('id',ParseIntPipe) id : number) : Promise<CentreEntity>{
+      return await this.centreService.remove(id);
     }
 }

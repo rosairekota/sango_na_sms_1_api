60e4cb5f330becaca62dc9c78af5dd5432ceae44
unknown <FranckTshiz27>
2021-08-15 23:35:06 +0100

    feat : add service class for child-registration.entity

--- src/child-registration/child-registration.controller.ts
+++ src/child-registration/child-registration.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ChildRegistrationEntity } from './child-registration.entity';
+import { ChildRegistrationService } from './child-registration.service';
+import { AddChildRegistrationDto } from './dto/add-child-registration.dto';
 
+@ApiTags('Child-registrations')
 @Controller('child-registration')
-export class ChildRegistrationController {}
+export class ChildRegistrationController {
+
+    constructor(private childRegistrationService : ChildRegistrationService){}
+    @Post()
+    async create(childRegistration : AddChildRegistrationDto) : Promise<ChildRegistrationEntity>{
+        return await this.childRegistrationService.add(childRegistration);
+    }
+}

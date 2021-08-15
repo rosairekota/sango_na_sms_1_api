f9641115f0cd0f63ad5d0f3767c11b64f751de9c
unknown <FranckTshiz27>
2021-08-15 23:40:53 +0100

    feat : add child-regitration controller

--- src/child-registration/child-registration.controller.ts
+++ src/child-registration/child-registration.controller.ts
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ChildRegistrationEntity } from './child-registration.entity';
 import { ChildRegistrationService } from './child-registration.service';
 import { AddChildRegistrationDto } from './dto/add-child-registration.dto';
+import { UpdateChildRegistrationDto } from './dto/update-child-registration.dto';
 
 @ApiTags('Child-registrations')
 @Controller('child-registration')
@@ -13,5 +14,20 @@ export class ChildRegistrationController {
     @Post()
     async create(childRegistration : AddChildRegistrationDto) : Promise<ChildRegistrationEntity>{
         return await this.childRegistrationService.add(childRegistration);
+    }
+
+    @Patch('/:id')
+    async edit(@Param('id',ParseIntPipe) id : number,childRegistration:UpdateChildRegistrationDto):Promise<ChildRegistrationEntity>{
+        return await this.childRegistrationService.update(id,childRegistration);
+    }
+
+    @Delete('/:id')
+    async remove(@Param('id',ParseIntPipe) id : number) : Promise<ChildRegistrationEntity>{
+        return await this.childRegistrationService.delete(id);
+    }
+
+    @Get()
+    async getAll():Promise<ChildRegistrationEntity[]>{
+        return await this.childRegistrationService.findAll();
     }
 }

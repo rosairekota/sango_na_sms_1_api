61b3338d9220cd2f9eb7894aa4a4fa96c59cafec
unknown <FranckTshiz27>
2021-08-16 11:09:20 +0100

    pull request

--- src/child-registration/dto/update-child-registration.dto.ts
+++ src/child-registration/dto/update-child-registration.dto.ts
@@ -3,7 +3,7 @@ import { ApiProperty, ApiTags } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional } from "class-validator";
 import { CentreEntity } from "src/centre/centre.entity";
 import { GenericValidatorMessages } from "src/helpers/generic-validator-message.dto";
-export class AddChildRegistrationDto extends GenericValidatorMessages{
+export class UpdateChildRegistrationDto extends GenericValidatorMessages{
 
     @IsOptional()
     @ApiProperty()

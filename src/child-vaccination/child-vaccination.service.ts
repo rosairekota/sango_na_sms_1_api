f303097dc3df87a64d5e429197e0990b23f97a93
rkota <rosairekota@gmail.com>
2021-08-18 08:29:27 +0100

    feat:add the services in the child vaccination module

--- src/child-vaccination/child-vaccination.service.ts
+++ src/child-vaccination/child-vaccination.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ChildVaccinationEntity } from './child-vaccination.entity';
+import { UpdateChildVaccinationDto } from './dto/update-child-vaccination.dto';
+import { AddChildVaccinationDto } from './dto/add-child-vaccination.dto';
 
 @Injectable()
-export class ChildVaccinationService {}
+export class ChildVaccinationService {
+  constructor(
+    @InjectRepository(ChildVaccinationEntity)
+    private readonly childVaccinationRepository: Repository<ChildVaccinationEntity>,
+  ) {}
+  async findAll(): Promise<ChildVaccinationEntity[]> {
+    return await this.childVaccinationRepository.find();
+  }
+
+  async add(
+    id: number,
+    entity: AddChildVaccinationDto,
+  ): Promise<ChildVaccinationEntity> {
+    return await this.childVaccinationRepository.save(entity);
+  }
+
+  async findById(id: number): Promise<ChildVaccinationEntity> {
+    const childVaccination = await this.childVaccinationRepository.findOne(id);
+    if (!childVaccination)
+      throw new NotFoundException(
+        "Cet enregistrement n'existe pas dans le système",
+      );
+    return childVaccination;
+  }
+
+  async Update(
+    id: number,
+    entity: Partial<UpdateChildVaccinationDto>,
+  ): Promise<ChildVaccinationEntity> {
+    const childVaccination = await this.childVaccinationRepository.preload({
+      id,
+      ...entity,
+    });
+    if (!childVaccination)
+      throw new NotFoundException(
+        "Cet enregistrement n'existe pas dans le système",
+      );
+    return await this.childVaccinationRepository.save(childVaccination);
+  }
+  async delete(id: number) {
+    const childVaccination = await this.findById(id);
+    if (!childVaccination)
+      throw new NotFoundException(
+        "Cet enregistrement n'existe pas dans le système",
+      );
+    return await this.childVaccinationRepository.remove(childVaccination);
+  }
+}

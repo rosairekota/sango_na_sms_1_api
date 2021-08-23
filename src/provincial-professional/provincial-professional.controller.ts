0e33d41f5935bc80c11d6a5c910e52cc97fe31da
rkota <rosairekota@gmail.com>
2021-08-23 01:24:45 +0100

    feat:feat:add the provinceprofessional Controller at the ProvinceProfessional module

--- src/provincial-professional/provincial-professional.controller.ts
+++ src/provincial-professional/provincial-professional.controller.ts
@@ -9,6 +9,44 @@ import {
   Param,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { AddProncialProfessionalDto } from './dto/add-proncial-professional.dto';
+import { UpdateProncialProfessionalDto } from './dto/update-proncial-professional.dto';
+import { ProvincialProfessionalEntity } from './provincial-professional.entity';
+import { ProvincialProfessionalService } from './provincial-professional.service';
 
+@ApiTags('prossionel provincial:')
 @Controller('provincial-professional')
-export class ProvincialProfessionalController {}
+export class ProvincialProfessionalController {
+  constructor(
+    private readonly provProfessionalService: ProvincialProfessionalService,
+  ) {}
+  @Get()
+  async getAll(): Promise<ProvincialProfessionalEntity[]> {
+    return await this.provProfessionalService.findAll();
+  }
+  @Post()
+  async create(
+    @Body() dto: AddProncialProfessionalDto,
+  ): Promise<ProvincialProfessionalEntity> {
+    return await this.provProfessionalService.add(dto);
+  }
+
+  @Get(':id')
+  async getById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<ProvincialProfessionalEntity> {
+    return await this.provProfessionalService.findById(id);
+  }
+
+  @Get(':id')
+  async edit(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: Partial<UpdateProncialProfessionalDto>,
+  ): Promise<ProvincialProfessionalEntity> {
+    return await this.provProfessionalService.update(id, dto);
+  }
+  @Get(':id')
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return await this.provProfessionalService.delete(id);
+  }
+}

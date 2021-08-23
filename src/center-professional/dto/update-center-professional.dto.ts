175565b6de56baa3aff02df9be4b74a0a4b8aa3d
rkota <rosairekota@gmail.com>
2021-08-23 01:41:07 +0100

    feat:feat:add the CenterProfessional dtos at the CenterProfessional module

--- src/center-professional/dto/update-center-professional.dto.ts
+++ src/center-professional/dto/update-center-professional.dto.ts
@@ -1,1 +1,14 @@
-export class UpdateCenterProfessionalDto {}
+import { IsOptional } from 'class-validator';
+import { GenericValidatorMessages } from 'src/helpers/generic-validator-message.dto';
+import { ProfessionalEntity } from 'src/professional/professional.entity';
+import { CentreEntity } from 'src/centre/centre.entity';
+
+export class UpdateCenterProfessionalDto extends GenericValidatorMessages {
+  @IsOptional()
+  professional: ProfessionalEntity;
+
+  @IsOptional(
+    UpdateCenterProfessionalDto.genericEmptyMessage('le professionel'),
+  )
+  center: CentreEntity;
+}

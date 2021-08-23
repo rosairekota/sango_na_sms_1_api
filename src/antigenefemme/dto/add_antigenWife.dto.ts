3f556afce47f14a0aec6944e70892e9b43a9f5b7
rkota <rosairekota@gmail.com>
2021-08-23 12:14:48 +0100

    fix:resolve the validator in antigenewife dtos of antigenewife Module

--- src/antigenefemme/dto/add_antigenWife.dto.ts
+++ src/antigenefemme/dto/add_antigenWife.dto.ts
@@ -1,11 +1,11 @@
 /* eslint-disable prettier/prettier */
-import { IsEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 import { GenericValidatorMessages } from 'src/helpers/generic-validator-message.dto';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class AddAntigenWifeDto extends GenericValidatorMessages {
   @ApiProperty()
-  @IsEmpty(AddAntigenWifeDto.genericEmptyMessage("l'intitulé de l'antigène"))
+  @IsNotEmpty(AddAntigenWifeDto.genericEmptyMessage("l'intitulé de l'antigène"))
   antigen_title: string;
 
   @ApiProperty()
--- src/antigenefemme/dto/update_anitgenWife.dto.ts
+++ src/antigenefemme/dto/update_anitgenWife.dto.ts
@@ -1,11 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 import { GenericValidatorMessages } from 'src/helpers/generic-validator-message.dto';
 
 export class UpdateAntigenWifeDto extends GenericValidatorMessages {
   @ApiProperty()
-  @IsEmpty(UpdateAntigenWifeDto.genericEmptyMessage("l'intitulé de l'antigène"))
+  @IsNotEmpty(
+    UpdateAntigenWifeDto.genericEmptyMessage("l'intitulé de l'antigène"),
+  )
   antigen_title: string;
 
   @ApiProperty()

dce236578a6ea71b0aef8a06c852dc9ef8ef937e
rkota <rosairekota@gmail.com>
2021-08-30 12:06:49 +0100

    feat:configure the jwt module in userModule

--- src/auth/enum/roles.enum.ts
+++ src/auth/enum/roles.enum.ts
@@ -1,6 +1,7 @@
 export enum UserRoles {
   ROLE_SUPERUSER = 'super',
   ROLE_ADMINISTRATOR = 'admin',
-  ROLE_WRITER = 'user',
+  ROLE_PROFESSIONAL_CENTER = 'p_center',
+  ROLE_PROFESSIONAL_ZONE = 'p_zone',
   ROLE_USER = 'free_user',
 }

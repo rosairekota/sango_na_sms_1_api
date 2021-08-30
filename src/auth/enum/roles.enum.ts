2223e8ab639e07cbfedf2672d26748eaf87fbd10
rkota <rosairekota@gmail.com>
2021-08-30 14:55:35 +0100

    feat:add the resposible roles

--- src/auth/enum/roles.enum.ts
+++ src/auth/enum/roles.enum.ts
@@ -3,5 +3,6 @@ export enum UserRoles {
   ROLE_ADMINISTRATOR = 'admin',
   ROLE_PROFESSIONAL_CENTER = 'p_center',
   ROLE_PROFESSIONAL_ZONE = 'p_zone',
+  ROLE_RESPONSIBLE = 'responsable',
   ROLE_USER = 'free_user',
 }

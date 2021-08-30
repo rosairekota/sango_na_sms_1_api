5b84caff5c03c5821468ff60fa9f6c1e9d762230
rkota <rosairekota@gmail.com>
2021-08-30 13:37:36 +0100

    feat:add Signup, SignIn and getAll methods in userController

--- src/auth/user.controller.ts
+++ src/auth/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, ParseIntPipe, Body } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UserService } from './user.service';
+import { UserEntity } from './user.entity';
+import { AddUserDto } from './dto/add-user.dto';
+import { UserCredentialsDto } from './dto/user-credentials.dto';
 
-@Controller('user')
-export class UserController {}
+@ApiTags('User:')
+@Controller('api/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post('signup')
+  async signup(
+    @Body() newUser: Partial<AddUserDto>,
+  ): Promise<Partial<UserEntity>> {
+    return await this.userService.register(newUser);
+  }
+
+  @Post('login')
+  async signin(@Body() credentials: UserCredentialsDto) {
+    return await this.userService.login(credentials);
+  }
+  @Get()
+  async getAll(): Promise<UserEntity[]> {
+    return await this.userService.findAll();
+  }
+}

2934165cb4aa36e38842393d209c5f0a313f56c6
Eminence <mulemanowa@gmail.com>
2021-09-01 18:39:09 +0200

    fix moduls

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,10 +14,6 @@ import { ProvinceModule } from './province/province.module';
 import * as dotenv from 'dotenv';
 import { CheckExistZoneMiddleware } from './middlewares/check-exist-zone.middleware';
 import { CentreModule } from './centre/centre.module';
-<<<<<<< HEAD
-// import { ChildPeriodModule } from './child-period/child-period.module';
-=======
->>>>>>> 4539f70fa5f8a6cf4cdefc506e44b19ceb32c2a6
 import { ChildModule } from './child/child.module';
 import { WifeperiodModule } from './wifeperiod/wifeperiod.module';
 import { WomanInscriptionModule } from './woman-inscription/woman-inscription.module';
@@ -52,10 +48,6 @@ dotenv.config();
     AireModule,
     AntigenModule,
     CentreModule,
-<<<<<<< HEAD
-    // ChildPeriodModule,
-=======
->>>>>>> 4539f70fa5f8a6cf4cdefc506e44b19ceb32c2a6
     ChildModule,
     ChildRegistrationModule,
     ProfessionalModule,
--- src/woman-vaccination/dto/update-woman-vaccination.dto.ts
+++ src/woman-vaccination/dto/update-woman-vaccination.dto.ts
@@ -1,14 +1,7 @@
 /* eslint-disable prettier/prettier */
-<<<<<<< HEAD
-import { IsNotEmpty, IsOptional } from "class-validator";
-// import { AntigenefemmeEntity } from "src/antigenefemme/antigenefemme.entity";
-import { FemmeEntity } from "src/femme/femme.entity";
-import { GenericValidatorMessages } from "src/helpers/generic-validator-message.dto";
-=======
 import { IsNotEmpty, IsOptional } from 'class-validator';
 import { FemmeEntity } from 'src/femme/femme.entity';
 import { GenericValidatorMessages } from 'src/helpers/generic-validator-message.dto';
->>>>>>> 4539f70fa5f8a6cf4cdefc506e44b19ceb32c2a6
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateWomanVaccinationDto extends GenericValidatorMessages {

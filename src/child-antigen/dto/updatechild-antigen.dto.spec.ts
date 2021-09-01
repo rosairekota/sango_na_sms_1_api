2859eb712dcc8b47f072288134e5da24dc92f044
unknown <FranckTshiz27>
2021-09-01 08:33:26 +0100

    pull request

--- src/child-antigen/dto/updatechild-antigen.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UpdatechildAntigenDto } from './updatechild-antigen.dto';
-
-describe('UpdatechildAntigenDto', () => {
-  it('should be defined', () => {
-    expect(new UpdatechildAntigenDto()).toBeDefined();
-  });
-});

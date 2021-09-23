c7f3fba838aaa72bd6de182a272b0504f76455ac
unknown <francktshizbu27@gmail.com>
2021-09-23 13:18:56 +0100

    feat : make perfect filter statistic woman method

--- src/helpers/timestemp.entity.ts
+++ src/helpers/timestemp.entity.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
 export default abstract class TimesTempEntity {
-  @CreateDateColumn({ update: false,type:'date'})
+  @CreateDateColumn({ update: false})
   createdAt: Date;
 
   @UpdateDateColumn({ nullable: true })

911695904dda5915e4eae053d44a41cc2a7965e3
unknown <francktshizbu27@gmail.com>
2021-09-27 12:48:02 +0100

    fix : change join properties in vaccination_enfant search view-template

--- src/child-vaccination/child-vaccination.entity.ts
+++ src/child-vaccination/child-vaccination.entity.ts
@@ -36,7 +36,7 @@ export class ChildVaccinationEntity {
   size: number;
   @Column({ name: 'date_prevue_modifie',default:false })
   isDueDateModified:boolean;
-  @Column({ name: 'more_days',default:true,nullable:true })
+  @Column({ name: 'more_days',default:0,nullable:true })
   moreDays:number;
   @ManyToOne(() => ChildEntity, (child) => child.childVaccinations)
   child: ChildEntity;

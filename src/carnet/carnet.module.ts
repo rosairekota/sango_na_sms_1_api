3aa4abf324d5d9b2dbabe7c169b2a9dd99d71e7a
unknown <francktshizbu27@gmail.com>
2021-09-28 10:46:59 +0100

    feat : add vaccination object to carnetEntity

--- src/carnet/carnet.module.ts
+++ src/carnet/carnet.module.ts
@@ -11,7 +11,7 @@ import { ChildVaccinationService } from 'src/child-vaccination/child-vaccination
   imports: [
     TypeOrmModule.forFeature([CarnetEntity,ChildVaccinationEntity]),
   ],
-  providers: [CarnetService],
+  providers: [CarnetService,ChildVaccinationService],
   controllers: [CarnetController]
 })
 export class CarnetModule {}

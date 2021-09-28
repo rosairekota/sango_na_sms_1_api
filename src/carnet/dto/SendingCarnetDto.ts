c049d69a1b0c1169b04a9a8a8ad4c815a1fac851
unknown <francktshizbu27@gmail.com>
2021-09-28 11:35:09 +0100

    feat : add vaccination object to carnetEntity

--- src/carnet/dto/SendingCarnetDto.ts
+++ src/carnet/dto/SendingCarnetDto.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional} from "class-validator";
 import { ChildVaccinationEntity } from 'src/child-vaccination/child-vaccination.entity';
 import { CarnetEntity } from '../carnet.entity';
 export class SendingCarnetDto {

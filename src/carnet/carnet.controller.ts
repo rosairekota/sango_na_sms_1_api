e4722fe9b4d33f271ad4213d21740c56c5205d1a
unknown <francktshizbu27@gmail.com>
2021-09-28 11:36:13 +0100

    feat : add vaccination object to carnetEntity

--- src/carnet/carnet.controller.ts
+++ src/carnet/carnet.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarnetEntity } from './carnet.entity';
 import { CarnetService } from './carnet.service';
 import { ApiTags } from '@nestjs/swagger';
+import { SendingCarnetDto } from './dto/SendingCarnetDto';
 
 @ApiTags('carnet_enfant')
 @Controller('api/carnet')
@@ -10,7 +11,7 @@ export class CarnetController {
     constructor(private readonly carnetRepository: CarnetService) {}
 
     @Get(':id')
-     async getCarnetsByEnfant(@Param("id",ParseIntPipe) id:number ) :Promise<CarnetEntity[]>{
+     async getCarnetsByEnfant(@Param("id",ParseIntPipe) id:number ) :Promise<SendingCarnetDto[]>{
          return await this.carnetRepository.findCarnetsByEnfant(id);
      }
 }
